7afef8950dc06b446e3c490ec9e250ec94cc62c5
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-07-11 15:07:04 +0100

    Added attendance button to toggle attendance of logging in user to specific event on events show view.
    Display event attendees on event show view

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,10 +2,19 @@ class InvitationsController < ApplicationController
   def attend
     @event = Event.find(params[:id])
 
-    if @event.attendees.include?(current_user.id)
+    if @event.attendees.include?(current_user)
       redirect_to @event, notice: 'You are already attending'
     else
-      @event.attendees << current_user.id
+      @event.attendees << current_user
     end
+  end
+
+  def cancel_attend
+    @event = Event.find(params[:id])
+    @user = current_user
+
+    @event.attendees.delete(@user)
+
+    redirect_to @event, alert: 'Attendance cancelled!'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root 'events#index'
+  root to: 'events#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    member do
+      put '/events/:id', to: 'invitations#attend', as: :attend
+      delete 'events/:id', to: 'invitations#cancel_attend', as: :cancel_attend
+    end
+  end
   resources :users, only: [:show]
 end
