7f979b1a8e26752fbef05bf0bcbae37ef1c6858b
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-07-13 19:19:31 +0100

    Separated upcoming and past events in events index view using class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :invitations, foreign_key: :event_id
   has_many :attendees, through: :invitations
+
+  def self.upcoming
+    where("start > ?", DateTime.now)
+  end
+
+  def self.past
+    where("start < ?", DateTime.now)
+  end
 end
